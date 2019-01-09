7f013c2e7ddad7b41baa2da75ba66b17f42c7cb1
Frederik Funke <frederik-funke@gmx.de>
2019-01-09 02:57:22 +0100

    components for different views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { GroupComponent } from './group/group.component';
+import { GroupsComponent } from './groups/groups.component';
+import { CreateComponent } from './create/create.component';
+import { JoinComponent } from './join/join.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    GroupComponent,
+    GroupsComponent,
+    CreateComponent,
+    JoinComponent
   ],
   imports: [
     BrowserModule,
