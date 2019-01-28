ffcc48a00f94fe9bbe95f6f4531edb99ee5da72b
Frederik Funke <frederik-funke@gmx.de>
2019-01-28 23:48:26 +0100

    express path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "dist/secret-santa")))
+app.use(express.static(path.join(__dirname, "/dist/" /*"dist/secret-santa"*/)))
 app.use(bodyParser.urlencoded({extended: false}));
 
 // MONGOOSE
