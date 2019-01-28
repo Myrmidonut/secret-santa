0bc53bee4723eb780d09aca2e62b1ab2ed000850
Frederik Funke <frederik-funke@gmx.de>
2019-01-28 23:33:05 +0100

    process.env.PORT for heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passportLocalMongoose = require("passport-local-mongoose")
 require("dotenv").config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "dist/secret-santa")))
 app.use(bodyParser.urlencoded({extended: false}));
